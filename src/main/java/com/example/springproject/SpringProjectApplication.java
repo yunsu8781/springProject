d8241c6f35ce42137e3cf4dfcf47011847f6cfa9
Leeyunsu <lws8905@gmail.com>
2022-12-15 21:06:49 +0900

    Initial commit

---
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -0,0 +1,23 @@
+package com.example.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class SpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectApplication.class, args);
+
+
+    }
+    @RequestMapping
+    String home() {
+        return "hello world";
+    }
+
+}
